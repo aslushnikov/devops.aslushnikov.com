42cd2c618f92d87c003941e7fe82eab501cc2e73
Andrey Lushnikov <lushnikov@chromium.org>
2020-08-26 11:39:37 -0700

    fix: forgotten refactoring in databranch

--- cronjobs/databranch.js
+++ cronjobs/databranch.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
 const path = require('path');
 
-const {spawnAsync, spawnAsyncOrDie, makeTempDir} = require('./misc.js');
+const misc = require('./misc.js');
 
 const GITHUB_REPOSITORY = 'aslushnikov/devops.aslushnikov.com';
 
 class DataBranch {
   static async initialize(branch, cleanupHooks = []) {
-    const checkoutPath = await makeTempDir('devops-data-dir-tmp-folder-', cleanupHooks);
+    const checkoutPath = await misc.makeTempDir('devops-data-dir-tmp-folder-', cleanupHooks);
     console.log(`[databranch] initializing at ${checkoutPath}`);
     let url = `https://github.com/${GITHUB_REPOSITORY}.git`;
     // Use github authentication if we have access to it.
@@ -16,18 +16,18 @@ class DataBranch {
 
     await fs.promises.mkdir(checkoutPath, {recursive: true});
     // Check existance of a remote branch for this bot.
-    const {stdout} = await spawnAsync('git', 'ls-remote', '--heads', url, branch);
+    const {stdout} = await misc.spawn('git', 'ls-remote', '--heads', url, branch);
     // If there is no remote branch for this bot - create one.
     if (!stdout.includes(branch)) {
-      await spawnAsyncOrDie('git', 'clone', '--no-checkout', '--depth=1', url, checkoutPath);
+      await misc.spawnOrDie('git', 'clone', '--no-checkout', '--depth=1', url, checkoutPath);
 
-      await spawnAsyncOrDie('git', 'checkout', '--orphan', branch, {cwd: checkoutPath});
-      await spawnAsyncOrDie('git', 'reset', '--hard', {cwd: checkoutPath});
+      await misc.spawnOrDie('git', 'checkout', '--orphan', branch, {cwd: checkoutPath});
+      await misc.spawnOrDie('git', 'reset', '--hard', {cwd: checkoutPath});
     } else {
-      await spawnAsyncOrDie('git', 'clone', '--single-branch', '--branch', `${branch}`, '--depth=1', url, checkoutPath);
+      await misc.spawnOrDie('git', 'clone', '--single-branch', '--branch', `${branch}`, '--depth=1', url, checkoutPath);
     }
-    await spawnAsyncOrDie('git', 'config', 'user.email', `"github-actions@github.com"`, {cwd: checkoutPath});
-    await spawnAsyncOrDie('git', 'config', 'user.name', `"github-actions"`, {cwd: checkoutPath});
+    await misc.spawnOrDie('git', 'config', 'user.email', `"github-actions@github.com"`, {cwd: checkoutPath});
+    await misc.spawnOrDie('git', 'config', 'user.name', `"github-actions"`, {cwd: checkoutPath});
     return new DataBranch(checkoutPath, branch);
   }
 
@@ -54,15 +54,15 @@ class DataBranch {
 
   async upload(message = 'update data') {
     // Check if there's anything to update.
-    const {stdout} = await spawnAsyncOrDie('git', 'status', '-s', '--untracked-files=all', {cwd: this._checkoutPath});
+    const {stdout} = await misc.spawnOrDie('git', 'status', '-s', '--untracked-files=all', {cwd: this._checkoutPath});
     if (!stdout.trim()) {
       console.log('[databranch] FYI: no changes, nothing to upload');
       return;
     }
     console.log(`[databranch] Uploading data with message "${message}"`);
-    await spawnAsyncOrDie('git', 'add', '.', {cwd: this._checkoutPath});
-    await spawnAsyncOrDie('git', 'commit', '-m', message, '--author', '"github-actions <github-actions@github.com>"', {cwd: this._checkoutPath});
-    await spawnAsyncOrDie('git', 'push', 'origin', this._branch, {cwd: this._checkoutPath});
+    await misc.spawnOrDie('git', 'add', '.', {cwd: this._checkoutPath});
+    await misc.spawnOrDie('git', 'commit', '-m', message, '--author', '"github-actions <github-actions@github.com>"', {cwd: this._checkoutPath});
+    await misc.spawnOrDie('git', 'push', 'origin', this._branch, {cwd: this._checkoutPath});
   }
 }
 

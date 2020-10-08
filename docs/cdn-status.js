7474eb51c2f370db4fd6c778213aced52557a9b2
Andrey Lushnikov <lushnikov@chromium.org>
2020-10-08 16:13:23 -0700

    feat(site): full cdn status

--- docs/cdn-status.js
+++ docs/cdn-status.js
@@ -8,32 +8,63 @@ export async function fetchCDNStatus() {
   return fetch('https://raw.githubusercontent.com/aslushnikov/devops.aslushnikov.com/datastore--cdn-status/status.json').then(r => r.json()).then(json => {
     json.webkit.sort((a, b) => b.rev - a.rev);
     json.firefox.sort((a, b) => b.rev - a.rev);
+    json.chromium.sort((a, b) => b.rev - a.rev);
+    json.winldd.sort((a, b) => b.rev - a.rev);
+    json.ffmpeg.sort((a, b) => b.rev - a.rev);
     return json;
   });
 }
 
-export function renderWebkitCDNStatus(cdnData, preview = false) {
-  return renderBrowserStatus('WebKit', cdnData.webkit, cdnData.timestamp, preview);
-}
-
-export function renderFirefoxCDNStatus(cdnData, preview = false) {
-  return renderBrowserStatus('Firefox', cdnData.firefox, cdnData.timestamp, preview);
-}
-
-function renderBrowserStatus(browserName, infos, updateTimestamp, preview) {
-  const RECENT_RUNS = 3;
-  if (preview)
-    infos = infos.slice(0, RECENT_RUNS);
-
+export function renderCDNStatusPreview(cdnData) {
+  const rows = [
+    { name: 'Webkit', info: cdnData.webkit[0], },
+    { name: 'Firefox', info: cdnData.firefox[0], },
+    { name: 'Chromium', info: cdnData.chromium[0], },
+    { name: 'FFMPEG', info: cdnData.ffmpeg[0], },
+    { name: 'winldd', info: cdnData.winldd[0], },
+  ];
   return html`
     <section class=cdn-status>
       <hbox class=header>
         <div>
-          <h2>${browserName} CDN status</h2>
-          <div>(updated ${humanReadableTimeInterval(Date.now() - updateTimestamp)} ago)</div>
+          <h2>CDN status</h2>
+          <div>(updated ${humanReadableTimeInterval(Date.now() - cdnData.timestamp)} ago)</div>
         </div>
         <spacer></spacer>
-        ${preview && browserLogo(browserName)}
+      </hbox>
+      <section class=grid>
+        <vbox class="cell header"></vbox>
+        <vbox class="cell header">Linux</vbox>
+        <vbox class="cell header">Mac</vbox>
+        <vbox class="cell header">Win</vbox>
+        ${rows.map(({name, info}) => html`
+          <hbox class="cell revision">
+            ${name} r${info.rev}
+          </hbox>
+          <vbox class="cell">${renderLinuxURLs(info.urls)}</vbox>
+          <vbox class="cell">${renderMacURLs(info.urls)}</vbox>
+          <vbox class="cell">${renderWinURLs(info.urls)}</vbox>
+        `)}
+      </section>
+      <footer>
+        Full CDN status: ${rows.map(({name}) => html`
+          <a class="" href="/cdn-status-${name.toLowerCase()}.html">[${name}] </a>
+        `)}
+      </footer>
+    </section>
+  `;
+}
+
+export function renderCDNStatusFull(cdnData, appName) {
+  const infos = cdnData[appName.toLowerCase()];
+  return html`
+    <section class=cdn-status>
+      <hbox class=header>
+        <div>
+          <h2>${appName} CDN status</h2>
+          <div>(updated ${humanReadableTimeInterval(Date.now() - cdnData.timestamp)} ago)</div>
+        </div>
+        <spacer></spacer>
       </hbox>
       <section class=grid>
         <vbox class="cell header"></vbox>
@@ -47,11 +78,6 @@ function renderBrowserStatus(browserName, infos, updateTimestamp, preview) {
           <vbox class="cell">${renderWinURLs(info.urls)}</vbox>
         `)}
       </section>
-      ${preview && html`
-        <footer>
-          Showing ${RECENT_RUNS} most recent builds. <a href="/full-${browserName.toLowerCase()}-cdn-status.html">See all</a>
-        </footer>
-      `}
     </section>
   `;
 }

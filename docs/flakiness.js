49be65f2584f706fb2071d6ea3ec4ff30c414052
Andrey Lushnikov <lushnikov@chromium.org>
2020-11-23 12:22:06 -0600

    Revert "feat(flakiness): show the error when clicking on a test (#6)"
    This reverts commit 37f6c2eeaaff34d33f9d1bb4554d14ffc8887410.
    
    The new filters should provide all the same functionality.

--- docs/flakiness.js
+++ docs/flakiness.js
@@ -1,11 +1,9 @@
 import {html, svg} from './zhtml.js';
 import {humanReadableDate, browserLogoURL, browserLogo, commitURL} from './misc.js';
 import {SortButton, ExpandButton, Popover} from './widgets.js';
-import {parse} from './stack-trace.js';
 
 export async function fetchFlakiness() {
   return fetch('https://folioflakinessdashboard.blob.core.windows.net/dashboards/main.json').then(r => r.json()).then(json => {
-    console.log(json.buildbotRuns.flatMap(m => m.specs).filter(e => !e.ok));
     return json;
   });
 }
@@ -14,6 +12,8 @@ const popover = new Popover(document);
 document.documentElement.addEventListener('click', () => popover.hide(), false);
 
 export function renderFlakiness(data) {
+  console.log(data);
+
   const specIdToSpec = new Map();
   const specIdToShaToCellInfo = new Map();
   const shaToDetails = new Map();
@@ -126,8 +126,6 @@ export function renderFlakiness(data) {
         ${[...fileToSpecs].map(([file, specs]) => html`
           <div class=specfile>${file}</div>
           ${specs.map(spec => html`
-            <details>
-            <summary>
             <table-row>
               <spec-column>
                 <div class=specname>${spec.line}: ${spec.title}</div>
@@ -140,9 +138,6 @@ export function renderFlakiness(data) {
                   <commit-info onclick=${popover.onClickHandler(renderCommitInfo.bind(null, spec.specId, info))} class=${info.className}>${info.flakyTests.length + info.failingTests.length || ''}</commit-info>
               `)}
             </table-row>
-            </summary>
-            ${lazyRender(() => renderSpecErrors(spec))}
-            </details>
           `)}
         `)}
       </section>
@@ -194,164 +189,11 @@ export function renderFlakiness(data) {
     else
       info = html`<test-info class=none>n/a</test-info>`;
     return html`
-      <div>
-        <a href="${test.url}">${info} <test-name>${test.name}</test-name></a>
-        ${test.runs.map(run => renderRunError(run, test))}
-      </div>
+      <div><a href="${test.url}">${info}</a> ${test.name}</div>
     `;
-  }
-
-  function renderRunError(run, test) {
-    const {error, status} = run;
-    console.log(run, test);
-    if (status === 'passed')
-      return html``;
-    if (!error)
-      return html`<div>${status}</div>`;
-    
-    const message = error.stack || error.message || JSON.stringify(error);
-    const lines = String(message).split('\n');
-    if (lines.length === 1)
-      return html`<pre>${lines[0]}</pre>`;
-    return html`
-    <details>
-      <summary>${lines[0]}</summary>
-      ${lines.slice(1).join('\n')}
-    </details>
-    `;
-  }
-
-  function renderSpecErrors(spec) {
-    const infos = specIdToCommitsInfo.get(spec.specId);
-    const tests = infos.flatMap(info => [...info.flakyTests, ...info.failingTests]);
-    const runs = tests.flatMap(test => test.runs);
-    const errors = new Set();
-    for (const run of runs) {
-      if (!run.status || run.status === 'skipped' || run.status === 'passed')
-        continue;
-      if (run.error)
-        errors.add(JSON.stringify(run.error));
-      else
-        errors.add(JSON.stringify(run.status));
-    }
-    return html`
-    <error-description>
-      ${[...errors].map(errorString => renderError(JSON.parse(errorString), spec.file))}
-    </error-description>`;
-  }
-
-  function renderError(error, file) {
-    if (typeof error === 'string')
-      return html`${error}`;
-    if (!error.message)
-      return html`<pre>${JSON.stringify(error, undefined, 2)}</pre>`;
-    if (!error.stack)
-      return html`<pre>${highlightTerminalText(error.message)}</pre>`;
-    const tokens = [];
-    const messageLocation = error.stack.indexOf(error.message);
-    const preamble = error.stack.substring(0, messageLocation + error.message.length);
-    tokens.push(preamble);
-    const callsite = file ? callsiteInFile(error, file) : null;
-    if (callsite)
-      console.log(callsite);
-    return html`
-  <pre>${highlightTerminalText(preamble)}</pre>
-  ${lazyRender(async () => {
-    if (!callsite || !callsite.fileName)
-      return html``;
-    const [CodeMirror, contents] = await Promise.all([
-      getCodeMirror(),
-      getFile(callsite.fileName),
-    ]);
-    const host = document.createElement('div');
-    host.style.overflow = 'hidden';
-    const cm = CodeMirror(host, {
-      mode:  "javascript",
-      value: contents,
-      lineNumbers: true
-    });
-    cm.markText({
-        line: callsite.lineNumber - 1,
-        ch: 0,
-      }, {
-        line: callsite.lineNumber - 1,
-        ch: callsite.columnNumber - 1,
-      }, {
-        className: 'error'
-      }
-    );
-    setTimeout(() => {
-      const coords = cm.charCoords({
-        line: callsite.lineNumber,
-        ch: callsite.columnNumber,
-      }, 'local');
-      cm.refresh();
-      cm.scrollTo(coords.left - 460, (coords.top + coords.bottom)/2 - 58 - 4 );  
-    }, 0);
-    return host;
-  }, html`<codemirror-placeholder></codemirror-placeholder>`)}
-<pre>${highlightTerminalText(error.stack.substring(preamble.length + (error.stack[preamble.length] === '\n' ? 1 : 0)))}</pre>`;
   }
 
   return render();
-}
-
-async function getFile(file) {
-  // TODO remove playwright-specific stuff here
-  const bases = [
-    `/home/runner/work/playwright/playwright/`,
-    `/Users/runner/work/playwright/playwright/`,
-    // TODO windows base
-  ];
-  const base = bases.find(base => file.startsWith(base));
-  if (!base)
-    return '';
-  // TODO: replace master with commit hash from one of the runs
-  const response = await fetch(`https://raw.githubusercontent.com/microsoft/playwright/master/${file.substring(base.length)}`)
-  return await response.text();
-}
-function callsiteInFile(error, file) {
-  for (const callsite of parse(error)) {
-    if (callsite.fileName && callsite.fileName.endsWith(file))
-      return callsite;
-  }
-  return null; 
-}
-
-/**
- * 
- * @param {string} text 
- */
-function highlightTerminalText(text) {
-  if (!text.includes('\u001b'))
-    return html`${text}`;
-  let color = null;
-  return html`${text.split('\u001b').map((segment, index) => {
-    if (index !== 0) {
-      const matches = /^[[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/.exec(segment);
-      if (matches && matches.length) {
-        const match = matches[0];
-        segment = segment.slice(match.length);
-        const COLORS = {
-          '[30m': 'black',
-          '[31m': 'red',
-          '[32m': 'green',
-          '[33m': 'yellow',
-          '[34m': 'blue',
-          '[35m': 'magenta',
-          '[36m': 'cyan',
-          '[37m': '#999',
-        };
-        if (match in COLORS)
-          color = COLORS[match];
-        else
-          color = null;
-      }
-    }
-    if (!color)
-      return html`${segment}`;
-    return html`<span style="color:${color}">${segment}</span>`;
-  })}`;
 }
 
 function isHealthyTest(test) {
@@ -368,28 +210,5 @@ function isFlakyTest(test) {
 }
 
 function isFailingTest(test) {
-  return !test.runs.some(run => run.status === 'skipped' || run.status === test.expectedStatus);
-}
-
-function lazyRender(render, placeholder = document.createElement('div')) {
-  const observer = new IntersectionObserver(async events => {
-    if (!events.some(x => x.isIntersecting))
-      return;
-    observer.disconnect();
-    placeholder.replaceWith(await render());
-  });
-  observer.observe(placeholder);
-  return placeholder;
-}
-
-async function getCodeMirror() {
-  if (window.CodeMirror)
-    return CodeMirror;
-  await import('./codemirror/codemirror.js');
-  await import('./codemirror/javascript.js');
-  const link = document.createElement('link');
-  link.rel = 'stylesheet';
-  link.href = './codemirror/codemirror.css';
-  document.head.appendChild(link);
-  return CodeMirror;
+  return !test.runs.some(run => run.status === test.expectedStatus);
 }
